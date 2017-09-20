45d268789161d227ad51b85a189de664f8cdb914
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-09-20 11:25:03 +0100

    Don't use docker args

--- docker/teamcity-build.sh
+++ docker/teamcity-build.sh
@@ -11,8 +11,6 @@ APP_DOCKER_COMMIT_TAG=$REGISTRY/$NAME:$GIT_ID
 APP_DOCKER_BRANCH_TAG=$REGISTRY/$NAME:$GIT_BRANCH
 
 docker build \
-       --build-arg GIT_ID=$GIT_ID \
-       --build-arg GIT_BRANCH=$GIT_BRANCH \
        --tag $APP_DOCKER_COMMIT_TAG \
        --tag $APP_DOCKER_BRANCH_TAG \
        -f docker/Dockerfile .
