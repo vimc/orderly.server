4e3470744c3d65ae3204c9f6e110d97aaa7596ad
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-09-21 08:19:00 +0100

    Tidy up utilities

--- R/server.R
+++ R/server.R
@@ -289,20 +289,3 @@ parse_path <- function(x) {
 
   list(re = sprintf("^%s$", x), args = args)
 }
-
-as_logical <- function(x, name = deparse(substitute(x))) {
-  switch(tolower(x),
-         "true" = TRUE,
-         "false" = FALSE,
-         stop(sprintf("Invalid input for '%s'", name)))
-}
-
-to_json <- function(x, ...) {
-  jsonlite::toJSON(x, auto_unbox = TRUE, ...)
-}
-
-time_diff_secs <- function(t, t0) {
-  dt <- t - t0
-  units(dt) <- "secs"
-  as.integer(dt)
-}
