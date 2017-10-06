39d716d4bfe8402af1a352c63352fbf18023ecc5
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-10-06 14:58:30 +0100

    Fix user flag in examples

--- docker/use.R
+++ docker/use.R
@@ -1,2 +1,2 @@
 Rscript -e 'orderly:::prepare_orderly_example("interactive", "orderly")'
-docker run --rm -v ${PWD}/orderly:/orderly -p 8123:8123 --user docker docker.montagu.dide.ic.ac.uk:5000/orderly.server:i648 --help
+docker run --rm -v ${PWD}/orderly:/orderly -p 8123:8123 --user $UID docker.montagu.dide.ic.ac.uk:5000/orderly.server:i648 --help
