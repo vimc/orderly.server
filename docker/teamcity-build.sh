d8c36ab949bcceba40ecbf3b3d71566ca957a1d7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-04-02 08:52:01 +0100

    Push images to public registry too

--- docker/teamcity-build.sh
+++ docker/teamcity-build.sh
@@ -3,17 +3,27 @@ set -e
 
 GIT_ID=$(git rev-parse --short=7 HEAD)
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
-REGISTRY=docker.montagu.dide.ic.ac.uk:5000
+REGISTRY_PRIVATE=docker.montagu.dide.ic.ac.uk:5000
+REGISTRY_PUBLIC=vimc
 NAME=orderly.server
 
 APP_DOCKER_TAG=$REGISTRY/$NAME
-APP_DOCKER_COMMIT_TAG=$REGISTRY/$NAME:$GIT_ID
-APP_DOCKER_BRANCH_TAG=$REGISTRY/$NAME:$GIT_BRANCH
+APP_PUBLIC_COMMIT_TAG=$REGISTRY_PUBLIC/$NAME:$GIT_ID
+APP_PUBLIC_BRANCH_TAG=$REGISTRY_PUBLIC/$NAME:$GIT_BRANCH
+APP_PRIVATE_COMMIT_TAG=$REGISTRY_PRIVATE/$NAME:$GIT_ID
+APP_PRIVATE_BRANCH_TAG=$REGISTRY_PRIVATE/$NAME:$GIT_BRANCH
 
 docker build --pull \
-       --tag $APP_DOCKER_COMMIT_TAG \
-       --tag $APP_DOCKER_BRANCH_TAG \
+       --tag $APP_PUBLIC_DOCKER_COMMIT_TAG \
+       --tag $APP_PUBLIC_DOCKER_BRANCH_TAG \
+       --tag $APP_PRIVATE_DOCKER_COMMIT_TAG \
+       --tag $APP_PRIVATE_DOCKER_BRANCH_TAG \
        -f docker/Dockerfile .
 
-docker push $APP_DOCKER_BRANCH_TAG
-docker push $APP_DOCKER_COMMIT_TAG
+## NOTE: we tag and push to the public (vimc) registry as well as our
+## private docker registry in order to make testing of the orderly web
+## deploy tool easier.
+docker push $APP_PUBLIC_DOCKER_BRANCH_TAG
+docker push $APP_PUBLIC_DOCKER_COMMIT_TAG
+docker push $APP_PRIVATE_DOCKER_BRANCH_TAG
+docker push $APP_PRIVATE_DOCKER_COMMIT_TAG
