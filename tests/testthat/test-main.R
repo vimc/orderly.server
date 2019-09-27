124b05b4423a60ada88d406187e113ba8d5506cb
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-09-27 14:19:06 +0100

    Add tests of the main script

---
+++ tests/testthat/test-main.R
@@ -0,0 +1,50 @@
+context("main")
+
+test_that("defaults", {
+  expect_equal(
+    main_args("path"),
+    list(path = "path", port = 8321, host = "0.0.0.0", allow_ref = TRUE,
+         go_signal = NULL))
+})
+
+
+test_that("set port", {
+  expect_equal(
+    main_args(c("path", "--port", "8888")),
+    list(path = "path", port = 8888, host = "0.0.0.0", allow_ref = TRUE,
+         go_signal = NULL))
+})
+
+
+test_that("set host", {
+  expect_equal(
+    main_args(c("path", "--host", "127.0.0.1")),
+    list(path = "path", port = 8321, host = "127.0.0.1", allow_ref = TRUE,
+         go_signal = NULL))
+})
+
+
+test_that("prevent reference switch", {
+  expect_equal(
+    main_args(c("path", "--no-ref")),
+    list(path = "path", port = 8321, host = "0.0.0.0", allow_ref = FALSE,
+         go_signal = NULL))
+})
+
+
+test_that("Set go signal", {
+  expect_equal(
+    main_args(c("path", "--go-signal", "somewhere")),
+    list(path = "path", port = 8321, host = "0.0.0.0", allow_ref = TRUE,
+         go_signal = "somewhere"))
+})
+
+
+test_that("write script", {
+  p <- tempfile()
+  expect_error(write_script(p), "'path' must be a directory")
+  dir.create(p)
+  res <- write_script(p)
+  expect_equal(basename(res), "orderly.server")
+  expect_true(file.exists(res))
+})
