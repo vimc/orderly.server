3d3f4b2f21c28e1cbbdb5bbb91d3aede50d09960
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-05 18:48:27 +0100

    Remove helper created in f77d63b

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -12,13 +12,3 @@ test_that("as_integer", {
   expect_error(as_integer("-1"), "Invalid input for")
   expect_error(as_integer("1.23"), "Invalid input for")
 })
-
-
-test_that("as_orderly_parameter_list", {
-  expect_null(as_orderly_parameter_list(NA))
-  expect_null(as_orderly_parameter_list(NULL))
-  expect_null(as_orderly_parameter_list("{}"))
-  expect_equal(as_orderly_parameter_list('{"a":1}'), "a=1")
-  expect_equal(as_orderly_parameter_list('{"a":"foo"}'), "a=foo")
-  expect_equal(as_orderly_parameter_list('{"a":1,"b":2}'), c("a=1", "b=2"))
-})
